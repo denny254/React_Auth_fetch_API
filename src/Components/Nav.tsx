b661cdb60881578bcbcf8a5c00b5b72be113787f
Dennis Kinanga <denniskinanga6@gmail.com>
2024-11-01 23:15:28 +0300

    Auth

---
+++ src/Components/Nav.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface NavProps {
+    name: string;
+    setName: (name: string) => void;
+}
+
+const Nav: React.FC<NavProps> = (props) => {
+
+    const Logout = async () => {
+        await fetch('http://localhost:8000/api/logout', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+        });
+
+        props.setName('');
+    }
+
+    let menu;
+
+    if (props.name === '') {
+        menu = (
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item active">
+                    <Link to="/login" className="nav-link">Login</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/register" className="nav-link active">Register</Link>
+                </li>
+            </ul>
+        )
+    } else {
+        menu = (
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item active">
+                    <Link to="/login" className="nav-link" onClick={Logout}>Logout</Link>
+                </li>
+            </ul>
+        )
+    }
+
+    return (
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand">Home</Link>
+                <div>{menu}</div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
